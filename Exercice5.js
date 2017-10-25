033f650c635bcda0df14b435d5da5f014f4458ec
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-10-25 08:57:18 +0200

    Changement du math.round en math.floor

--- Exercice5.js
+++ Exercice5.js
@@ -1,5 +1,5 @@
 function form2() {
   var premier_nombre = document.getElementById('premier_nombre').value;
   var deuxieme_nombre = document.getElementById('deuxieme_nombre').value;
-  alert(parseInt(premier_nombre * deuxieme_nombre));
+  alert(Math.floor(premier_nombre) * deuxieme_nombre);
 }
