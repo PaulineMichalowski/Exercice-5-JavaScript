041cc1be746b3d53402ba09b9405264886d14a8b
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-10-25 14:51:03 +0200

    Correction de l'exercice 5

--- Exercice5.js
+++ Exercice5.js
@@ -1,5 +1,9 @@
-function form2() {
-  var premier_nombre = document.getElementById('premier_nombre').value;
-  var deuxieme_nombre = document.getElementById('deuxieme_nombre').value;
-  alert(Math.floor(premier_nombre) * deuxieme_nombre);
-}
+function donnee(){
+   var premier_nombre = document.getElementById('premier_nombre').value;
+   var deuxieme_nombre = document.getElementById('deuxieme_nombre').value;
+//   result = parseInt(premier_nombre) * deuxieme_nombre;
+   if(isNaN(premier_nombre) == false && isNaN(deuxieme_nombre) == false){
+        result = Math.trunc(premier_nombre) * deuxieme_nombre;
+        alert(result);
+     }
+ }
